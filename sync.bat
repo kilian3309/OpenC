269dd00552f4f0542bfbdb81517a67c2675e2aff
kilian3309 <kiki.jugie@gmail.com>
2017-05-26 20:00:13 +0200

    ''

--- sync.bat
+++ sync.bat
@@ -1,3 +1,4 @@
-"C:\Program Files\Git\bin\git.exe" commit -a --allow-empty-message -m ''
+set /P msg="Message (can be void): "
+"C:\Program Files\Git\bin\git.exe" commit -a --allow-empty-message -m '%msg%'
 "C:\Program Files\Git\bin\git.exe" push
 pause
